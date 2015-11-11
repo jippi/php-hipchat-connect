0c2b8e8249de084e22dfd1b65b237998c9a4607c
Christian Winther <cw@bownty.com>
2015-11-11 15:13:03 +0000

    remove stuff from cakephp

--- Exception/HipChatException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace HipChat\Exception;
-
-use Exception;
-
-class HipChatException extends Exception {
-
-}
--- config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Cake\Routing\Router;
-
-Router::plugin('HipChat', function ($routes) {
-    $routes->fallbacks('InflectedRoute');
-});
